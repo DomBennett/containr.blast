10d79c651f060f628b7b92a081656a33de460ad0
Dominic Bennett <dominic.john.bennett@gmail.com>
2019-09-10 16:46:10 +0200

    swtich to arglist

--- R/functions.R
+++ R/functions.R
@@ -1,6 +1,5 @@
 
-base_function <- function(cmd, ...) {
-  arglist <- arglist_get(...)
+base_function <- function(cmd, arglist) {
   possible_files <- c(arglist, paste0(arglist, '.nhr'), paste0(arglist, '.nin'),
                       paste0(arglist, '.nsq'))
   files_to_send <- filestosend_get(arglist = possible_files)
@@ -13,199 +12,199 @@ base_function <- function(cmd, ...) {
 #' @name makeblastdb
 #' @title makeblastdb
 #' @description Run makeblastdb
-#' @param ... Arguments
+#' @param arglist Arguments
 #' @example examples/makeblastdb.R
 #' @export
-makeblastdb <- function(...) {
-  base_function(cmd = 'makeblastdb', ...)
+makeblastdb <- function(arglist = arglist_get(...)) {
+  base_function(cmd = 'makeblastdb', arglist = arglist)
 }
 
 #' @name blastn
 #' @title blastn
 #' @description Run blastn
-#' @param ... Arguments
+#' @param arglist Arguments
 #' @example examples/blastn.R
 #' @export
-blastn <- function(...) {
-  base_function(cmd = 'blastn', ...)
+blastn <- function(arglist = arglist_get(...)) {
+  base_function(cmd = 'blastn', arglist = arglist)
 }
 
 #' @name blast_formatter
 #' @title blast_formatter
 #' @description Run blast_formatter
-#' @param ... Arguments
+#' @param arglist Arguments
 #' @example examples/blast_formatter.R
 #' @export
-blast_formatter <- function(...) {
-  base_function(cmd = 'blast_formatter', ...)
+blast_formatter <- function(arglist = arglist_get(...)) {
+  base_function(cmd = 'blast_formatter', arglist = arglist)
 }
 
 #' @name blastdbcmd
 #' @title blastdbcmd
 #' @description Run blastdbcmd
-#' @param ... Arguments
+#' @param arglist Arguments
 #' @example examples/blastdbcmd.R
 #' @export
-blastdbcmd <- function(...) {
-  base_function(cmd = 'blastdbcmd', ...)
+blastdbcmd <- function(arglist = arglist_get(...)) {
+  base_function(cmd = 'blastdbcmd', arglist = arglist)
 }
 
 #' @name blastx
 #' @title blastx
 #' @description Run blastx
-#' @param ... Arguments
+#' @param arglist Arguments
 #' @example examples/blastx.R
 #' @export
-blastx <- function(...) {
-  base_function(cmd = 'blastx', ...)
+blastx <- function(arglist = arglist_get(...)) {
+  base_function(cmd = 'blastx', arglist = arglist)
 }
 
 #' @name dustmasker
 #' @title dustmasker
 #' @description Run dustmasker
-#' @param ... Arguments
+#' @param arglist Arguments
 #' @example examples/dustmasker.R
 #' @export
-dustmasker <- function(...) {
-  base_function(cmd = 'dustmasker', ...)
+dustmasker <- function(arglist = arglist_get(...)) {
+  base_function(cmd = 'dustmasker', arglist = arglist)
 }
 
 #' @name makembindex
 #' @title makembindex
 #' @description Run makembindex
-#' @param ... Arguments
+#' @param arglist Arguments
 #' @example examples/makembindex.R
 #' @export
-makembindex <- function(...) {
-  base_function(cmd = 'makembindex', ...)
+makembindex <- function(arglist = arglist_get(...)) {
+  base_function(cmd = 'makembindex', arglist = arglist)
 }
 
 #' @name rpsblast
 #' @title rpsblast
 #' @description Run rpsblast
-#' @param ... Arguments
+#' @param arglist Arguments
 #' @example examples/rpsblast.R
 #' @export
-rpsblast <- function(...) {
-  base_function(cmd = 'rpsblast', ...)
+rpsblast <- function(arglist = arglist_get(...)) {
+  base_function(cmd = 'rpsblast', arglist = arglist)
 }
 
 #' @name tblastn
 #' @title tblastn
 #' @description Run tblastn
-#' @param ... Arguments
+#' @param arglist Arguments
 #' @example examples/tblastn.R
 #' @export
-tblastn <- function(...) {
-  base_function(cmd = 'tblastn', ...)
+tblastn <- function(arglist = arglist_get(...)) {
+  base_function(cmd = 'tblastn', arglist = arglist)
 }
 
 #' @name windowmasker
 #' @title windowmasker
 #' @description Run windowmasker
-#' @param ... Arguments
+#' @param arglist Arguments
 #' @example examples/windowmasker.R
 #' @export
-windowmasker <- function(...) {
-  base_function(cmd = 'windowmasker', ...)
+windowmasker <- function(arglist = arglist_get(...)) {
+  base_function(cmd = 'windowmasker', arglist = arglist)
 }
 
 #' @name blastdb_aliastool
 #' @title blastdb_aliastool
 #' @description Run blastdb_aliastool
-#' @param ... Arguments
+#' @param arglist Arguments
 #' @example examples/blastdb_aliastool.R
 #' @export
-blastdb_aliastool <- function(...) {
-  base_function(cmd = 'blastdb_aliastool', ...)
+blastdb_aliastool <- function(arglist = arglist_get(...)) {
+  base_function(cmd = 'blastdb_aliastool', arglist = arglist)
 }
 
 #' @name convert2blastmask
 #' @title convert2blastmask
 #' @description Run convert2blastmask
-#' @param ... Arguments
+#' @param arglist Arguments
 #' @example examples/convert2blastmask.R
 #' @export
-convert2blastmask <- function(...) {
-  base_function(cmd = 'convert2blastmask', ...)
+convert2blastmask <- function(arglist = arglist_get(...)) {
+  base_function(cmd = 'convert2blastmask', arglist = arglist)
 }
 
 #' @name makeprofiledb
 #' @title makeprofiledb
 #' @description Run makeprofiledb
-#' @param ... Arguments
+#' @param arglist Arguments
 #' @example examples/makeprofiledb.R
 #' @export
-makeprofiledb <- function(...) {
-  base_function(cmd = 'makeprofiledb', ...)
+makeprofiledb <- function(arglist = arglist_get(...)) {
+  base_function(cmd = 'makeprofiledb', arglist = arglist)
 }
 
 #' @name rpstblastn
 #' @title rpstblastn
 #' @description Run rpstblastn
-#' @param ... Arguments
+#' @param arglist Arguments
 #' @example examples/rpstblastn.R
 #' @export
-rpstblastn <- function(...) {
-  base_function(cmd = 'rpstblastn', ...)
+rpstblastn <- function(arglist = arglist_get(...)) {
+  base_function(cmd = 'rpstblastn', arglist = arglist)
 }
 
 #' @name tblastx
 #' @title tblastx
 #' @description Run tblastx
-#' @param ... Arguments
+#' @param arglist Arguments
 #' @example examples/tblastx.R
 #' @export
-tblastx <- function(...) {
-  base_function(cmd = 'tblastx', ...)
+tblastx <- function(arglist = arglist_get(...)) {
+  base_function(cmd = 'tblastx', arglist = arglist)
 }
 
 #' @name blastdbcheck
 #' @title blastdbcheck
 #' @description Run blastdbcheck
-#' @param ... Arguments
+#' @param arglist Arguments
 #' @example examples/blastdbcheck.R
 #' @export
-blastdbcheck <- function(...) {
-  base_function(cmd = 'blastdbcheck', ...)
+blastdbcheck <- function(arglist = arglist_get(...)) {
+  base_function(cmd = 'blastdbcheck', arglist = arglist)
 }
 
 #' @name blastp
 #' @title blastp
 #' @description Run blastp
-#' @param ... Arguments
+#' @param arglist Arguments
 #' @example examples/blastp.R
 #' @export
-blastp <- function(...) {
-  base_function(cmd = 'blastp', ...)
+blastp <- function(arglist = arglist_get(...)) {
+  base_function(cmd = 'blastp', arglist = arglist)
 }
 
 #' @name deltablast
 #' @title deltablast
 #' @description Run deltablast
-#' @param ... Arguments
+#' @param arglist Arguments
 #' @example examples/deltablast.R
 #' @export
-deltablast <- function(...) {
-  base_function(cmd = 'deltablast', ...)
+deltablast <- function(arglist = arglist_get(...)) {
+  base_function(cmd = 'deltablast', arglist = arglist)
 }
 
 #' @name psiblast
 #' @title psiblast
 #' @description Run psiblast
-#' @param ... Arguments
+#' @param arglist Arguments
 #' @example examples/psiblast.R
 #' @export
-psiblast <- function(...) {
-  base_function(cmd = 'psiblast', ...)
+psiblast <- function(arglist = arglist_get(...)) {
+  base_function(cmd = 'psiblast', arglist = arglist)
 }
 
 #' @name segmasker
 #' @title segmasker
 #' @description Run segmasker
-#' @param ... Arguments
+#' @param arglist Arguments
 #' @example examples/segmasker.R
 #' @export
-segmasker <- function(...) {
-  base_function(cmd = 'segmasker', ...)
+segmasker <- function(arglist = arglist_get(...)) {
+  base_function(cmd = 'segmasker', arglist = arglist)
 }
